d0e2fd2d299f4e1e61c95a3d4000b450dc23ebc7
Jose Torres <torres@balameb.com>
2015-06-14 23:49:24 -0600

    Styling titles

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Hospital Quality
 Hospital Quality exploratory data analysis for the Johns Hopkins specialisation. 
 ***
-## Data Source  
+### Data Source  
 
   
 The data for this assignment come from the Hospital Compare web site (http://hospitalcompare.hhs.gov) run by the U.S. Department of Health and Human Services. The purpose of the web site is to provide data and information about the quality of care at over 4,000 Medicare-certied hospitals in the U.S. This dataset essentially covers all major U.S. hospitals. This dataset is used for a variety of purposes, including determining whether hospitals should be fined for not providing high quality care to patients (see http://goo.gl/jAXFX for some background on this particular topic).
 
-## Objectives
+### Objectives
 1. Plot the 30 day mortality rates for heart attacks
 2. Find the best hospital in state
 3. Ranking  hospitals by outcome in a states
--- rankall.R
+++ rankall.R
@@ -1,38 +1,57 @@
-## rankall.R
+num_helper <- function(state_subset, col_num, num) {
+  # get "attack", "failure" and "pneumonia" vector
+  outcome_arr <- as.numeric(state_subset[, col_num])
+  len <- dim(state_subset[!is.na(outcome_arr), ])[1]
+  if (num == "best") {
+    rank <- rank_helper(state_subset, outcome_arr, 1)
+  } else if (num == "worst") {
+    rank <- rank_helper(state_subset, outcome_arr, len)
+  } else if (num > len) {
+    rank <- NA
+  } else {
+    rank <- rank_helper(state_subset, outcome_arr, num)
+  }
+  result <- rank
+  return(result)
+}
+
+rank_helper <- function(state_subset, outcome_arr, num) {
+  result <- state_subset[, 2][order(outcome_arr, state_subset[, 2])[num]]
+  return(result)
+}
 
 rankall <- function(outcome, num = "best") {
   ## Read outcome data
-  data <- read.csv("./ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
-  
   ## Check that state and outcome are valid
-  validOutcome = c("heart attack","heart failure","pneumonia")
-  if (!outcome %in% validOutcome) { stop("invalid outcome")}
-  
-  validState = sort(unique(data[,7]))
-  if (!state %in% validState) stop("invalid state")
-  
-  ## convert outcome name into column name
-  fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
-  colName <- fullColName[match(outcome,validOutcome)]
-  
   ## For each state, find the hospital of the given rank
-  hospital<-character(0)
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
   
-  for (i in seq_along(validState)) {
-    ## Return hospital name in that state with the given rank 30-day death rate
-    data.state <- data[data$State==validState[i],]
-    
-    # order data by outcome
-    sorted.data.state <- data.state[order(as.numeric(data.state[[colName]]),data.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
-    
-    #handle num input
-    this.num = num
-    if (this.num=="best") this.num = 1
-    if (this.num=='worst') this.num = nrow(sorted.data.state)
-    
-    hospital[i] <- sorted.data.state[this.num,"Hospital.Name"]
+  # read the data file
+  directory <- "data/outcome-of-care-measures.csv"
+  data <- read.csv(directory, colClasses="character")
+  valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
+  state_arr <- sort(unique(data$State))
+  arr_len <- length(state_arr)
+  hospital <- rep("", arr_len)
+  
+  if (!outcome %in% valid_outcomes) {
+    stop("invalid outcome")
+  } else {
+    for(i in 1:arr_len) {
+      # loop for each state
+      state_subset <- data[data[, 7]==state_arr[i], ]
+      if(outcome == "heart attack") {
+        hospital[i] <- num_helper(state_subset, 11, num) 
+      } else if (outcome == "heart failure") {
+        hospital[i] <- num_helper(state_subset, 17, num) 
+      } else {
+        hospital[i] <- num_helper(state_subset, 23, num) 
+      }
+    }
   }
-  
-  ## Return a data frame with the hospital names and the (abbreviated) state name
-  data.frame(hospital=hospital,state=validState,row.names=validState)
+  # create the data frame to return
+  df <- data.frame(hospital=hospital, state=state_arr)
+  result <- df
+  return(result)
 }
